239642859e8d8ae46bc13954377630913ef35cbe
root <root@debian>
2021-01-10 15:52:14 +0800

    提交者：root <root@debian>
      位于分支 master
      您的分支与上游分支 'origin/master' 一致。
    
      要提交的变更：
     	修改：     routes/user.js

--- routes/user.js
+++ routes/user.js
@@ -4,24 +4,26 @@ const pool = require("../pool.js"); //引入mysql连接池
 const add_user = async (req,res) => {
     try {
         const user = req.body;//获取前端传来的用户信息
-
-        pool.query("INSERT INTO user(uname,upwd) VALUES(?,?)",[ user.username, user.password ],(err,result)=>{
-            if(err){  //如果错误，则打印错误
-                res.json({type: "error",msg:"插入失败"});
-                console.error(err);
-            }else{
-                if(result.affectedRows>0){//进行数据库插入后返回的结果，如果result.affectedRows>0则会插入成功
-                    res.json({type: "success",msg: "插入成功"});
+        console.log(user);
+        if (user.username && user.password) {
+            pool.query("INSERT INTO user(uname,upwd) VALUES(?,?)",[ user.username, user.password ],(err,result)=>{
+                if(err){  //如果错误，则打印错误
+                    res.json({type: "error",msg:"用户已存在"});
                 }else{
-                    res.json({type: "error",msg: "插入失败"});
+                    if(result.affectedRows>0){//进行数据库插入后返回的结果，如果result.affectedRows>0则会插入成功
+                        res.json({type: "success",msg: "插入成功"});
+                    }else{
+                        res.json({type: "error",msg: "用户已存在"});
+                    }
                 }
-            }
-        });
+            });
+        }else{
+            res.json({type: "error",msg: "请保证数据的格式或位数正确"});
+        }
 
     } catch (error) {
         res.json({type: "error",msg: "错误:" + error});
     }
-    console.log(req.body);
 }
 
 module.exports = {
